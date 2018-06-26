9e56fd5998193ef9ad4a02c6e25a30f0ce7f74d5
Baccatore <yuichiro.suga.5@gmail.com>
2018-06-27 00:22:00 +0900

    Staged mid files

--- cell_count.pyx
+++ cell_count.pyx
@@ -1,52 +1,51 @@
 #!/usr/bin/env python3
 #coding:utf8
-#cython: language_level=3, boundscheck=False
+#cython:language_level=3, boundscheck=False
 import time
 from libcpp.vector cimport vector
+import itertools
+import numpy as np
+cimport numpy as np
 cimport cython
 
-def count(vector[:,:] xyz, \
-        vector[unsigned short] candidate_range, \
+def count(vector[vector[short]]xyz, \
+        vector[unsigned short]candidate_range, \
         vector[unsigned short] candidate_size):
-    cdef float t1, t2
-    cdef unsigned short i, j, k
+    cdef int i, j, k
     cdef unsigned int total_xyz, current_xyz, ijk
-    cdef vector[unsigned long] nb_cell_in_candidate_ijk
-    cdef vector[unsigned short]cell_xyz
-    cdef vector[unsigned short] trgt_grd
-    cdef vector[unsigned short] trgt_grd_rng
+    cdef vector[unsigned short] cell_xyz = [0]*3
+    cdef vector[unsigned short] trgt_grd_rng = [0]*3*2
+    cdef vector[long] nb_cell_in_candidate_ijk = [0]*np.prod(candidate_range)
+    #cdef time_point t1, t2
 
-    t1 = time.time()
+    t1 = time.clock()
     total_xyz = len(xyz)
     for current_xyz, cell_xyz in enumerate(xyz):
         #target grid zone
         for i in range(3):
-            trgt_grd[i] = cell_xyz[i] - candidate_size[i]
-        for i in range(3):
-            for j in range(2):
-                trgt_grd_rng[i+j*3] = 0
-        
-        for i in range(3):
-            #TODO revise following 3 lines
-            if trgt_grd_rng[i] < 0:
-                trgt_grd_rng[i] = 0
+            if cell_xyz[i] - candidate_size[i] < 0:
+                trgt_grd_rng[i]   = 0
                 trgt_grd_rng[i+3] = cell_xyz[i]
-            elif trgt_grd_rng[i+3] > candidate_range[i]:
-                trgt_grd_rng[i] = trgt_grd[i]
+            elif  cell_xyz[i] - candidate_size[i] > candidate_range[i]:
+                trgt_grd_rng[i]   = cell_xyz[i] - candidate_size[i]
                 trgt_grd_rng[i+3] = candidate_range[i]
-            else:
-                trgt_grd_rng[i] = trgt_grd[i]
+            else :
+                trgt_grd_rng[i]   = cell_xyz[i] - candidate_size[i]
                 trgt_grd_rng[i+3] = cell_xyz[i]
+        #TODO Assert range!
+        for i, j, k in itertools.product( \
+                range(trgt_grd_rng[0], trgt_grd_rng[3]), \
+                range(trgt_grd_rng[1], trgt_grd_rng[4]), \
+                range(trgt_grd_rng[2], trgt_grd_rng[5])):
+            ijk= i+j*candidate_range[0]+k*candidate_range[0]*candidate_range[1]
+            nb_cell_in_candidate_ijk[ijk] += 1
+        
+        for check in nb_cell_in_candidate_ijk[30000000:40000000]:
+            if check is not 0:
+                print(check)
 
-        for i in range(trgt_grd_rng[0], trgt_grd_rng[3]):
-            for j in range(trgt_grd_rng[1], trgt_grd_rng[4]):
-                for k in range(trgt_grd_rng[2], trgt_grd_rng[5]):
-                    ijk = i + j*candidate_range[0] + k*candidate_range[0]*candidate_range[1]
-                    nb_cell_in_candidate_ijk[ijk] +=1
-
-
-        if current_xyz%100 == 0:
-            t2 = time.time()
+        if current_xyz%10000 == 0:
+            t2 = time.clock()
             print('\r{0}/{1} Runtime: {2:.3f}     '.format(current_xyz,total_xyz, t2-t1),flush=True,end='')
             t1 = t2
     return nb_cell_in_candidate_ijk
--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+import numpy
+
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("cell_count", ["cell_count.pyx"],language='c++')]
+    ext_modules = [
+        Extension(
+            "cell_count",
+            ["cell_count.pyx"],
+            language='c++',
+            include_dirs=[numpy.get_include()]
+        )]
 )
