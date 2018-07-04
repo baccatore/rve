79f3bae173e200ffce13e593f0322a379092119b
Baccatore <yuichiro.suga.5@gmail.com>
2018-07-04 16:23:30 +0900

    Start parellelize

---
+++ util.py
@@ -0,0 +1,60 @@
+import re
+import pickle
+import glob
+import numpy as np
+from numba import jit
+
+def read_pbm(file_name):
+    xyz = []
+    result = re.search(r'\d+\.pbm$', file_name)
+    result = re.search(r'[0-9]+', result.group(0))
+    if result:
+        k = int(result.group(0))
+
+    with open(file_name,'r') as f:
+        if f.readline()[0:2] != 'P1':
+            #TODO raise error
+            print('\033[31mOnly P1 type pbm file is acceptable\033[m')
+            return
+        
+        for line in f:
+            if line[0] == '#':
+                continue
+            x_max, y_max = tuple(map(int,line.split()))
+            break
+
+        for j, line in enumerate(f):
+            #Array of cells in line y_i
+            yi = line.split()
+            f.readline()
+            for i, xiyj in enumerate(yi):
+                #Input is as string
+                if xiyj == '1':
+                    #Coordinate starts from 0
+                    xyz.append([i+1,j+1,k+1])
+    return xyz
+
+
+@jit
+def load_images(address):
+    xyz = []
+    file_list = glob.glob(address)
+    nb_file = len(file_list)
+    file_list.sort()
+    for i, file_name in enumerate(file_list):
+        xyz += read_pbm(file_name)
+        print('\rReading...', file_name, i+1, '/', nb_file, flush=True, end='')
+    return xyz
+
+
+@jit
+def write_xyz(file_name, xyz, binary=False):
+    print('\nWriting output...')
+    with open('result.xyz','w') as f:
+        for line in xyz:
+            coordinate = " ".join(map(str,line))
+            f.write(coordinate + "\n")
+    if binary:
+        print('Writing output in binary...')
+        with open('result_binary.xyz','wb') as f:
+           pickle.dump(xyz,f)
